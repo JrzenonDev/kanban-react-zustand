cd6a2ad6646a784ced06fc2f78706fab744a369e
JrzenonDev <jrobertoonb@gmail.com>
2023-10-24 00:39:02 -0300

    feat: create component Column and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Column from "./assets/components/Column";
 
 function App() {
   return (
-    <>
-      <div className="App"></div>
-    </>
+    <div className="App">
+      <Column state="PLANNED" />
+      <Column state="ONGOING" />
+      <Column state="DONE" />
+    </div>
   );
 }
 
