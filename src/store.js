0b4825d5988cb639047aa1829c5c1548ce28569b
JrzenonDev <jrobertoonb@gmail.com>
2023-10-25 03:10:21 -0300

    feat: create store and function deleteTask

--- src/store.js
+++ src/store.js
@@ -4,6 +4,11 @@ const store = (set) => ({
   tasks: [{ title: "Test task", state: "DONE" }],
   addTask: (title, state) =>
     set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+
+  deleteTask: (title) =>
+    set((store) => ({
+      tasks: store.tasks.filter((task) => task.title !== title),
+    })),
 });
 
 export const useStore = create(store);
