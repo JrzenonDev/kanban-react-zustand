176ea3601c5aaa830e092d06a664cc563f9f8182
JrzenonDev <jrobertoonb@gmail.com>
2023-10-25 11:45:54 -0300

    feat: creating store and move task functionality

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -6,15 +6,29 @@ import Task from "./Task";
 export default function Column({ state }) {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
+  const [drop, setDrop] = useState(false);
 
   const tasks = useStore((store) =>
     store.tasks.filter((task) => task.state === state)
   );
 
   const addTask = useStore((store) => store.addTask);
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+  const draggedTask = useStore((store) => store.draggedTask);
+  const moveTask = useStore((store) => store.moveTask);
 
   return (
-    <div className="column">
+    <div
+      className="column"
+      onDragOver={(e) => {
+        setDrop(true);
+        e.preventDefault();
+      }}
+      onDrop={() => {
+        moveTask(draggedTask, state);
+        setDraggedTask(null);
+      }}
+    >
       <div className="titleWrapper">
         <p>{state}</p>
         <button
--- src/store.js
+++ src/store.js
@@ -1,13 +1,25 @@
 import { create } from "zustand";
 
 const store = (set) => ({
+  // Initial store values
   tasks: [{ title: "Test task", state: "DONE" }],
+  draggedTask: null,
+
+  // Actions
   addTask: (title, state) =>
     set((store) => ({ tasks: [...store.tasks, { title, state }] })),
-
   deleteTask: (title) =>
     set((store) => ({
       tasks: store.tasks.filter((task) => task.title !== title),
+    })),
+
+  // set draggedTask
+  setDraggedTask: (title) => set({ draggedTask: title }),
+  moveTask: (title, state) =>
+    set((store) => ({
+      tasks: store.tasks.map((task) =>
+        task.title === title ? { title, state } : task
+      ),
     })),
 });
 
